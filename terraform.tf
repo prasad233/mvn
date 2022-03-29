d7a2dff2414c742fc0f96da0c949d76ddcebb99e
root <root@ip-172-31-22-34.ec2.internal>
2022-03-29 11:21:00 +0000

    second commit

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+provider aws {
+region = "us-east-1"
+access_key =
+secret_key =
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
